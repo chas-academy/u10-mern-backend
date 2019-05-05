d1d377225b7e1b7b60bb7584fa7907b6b52515ed
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-05-06 01:27:36 +0200

    Change route to /staging.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Moment 23!!!'));
+app.get('/staging', (req, res) => res.send('Hello Moment 23!!!'));
 
 app.listen(port, () => console.log(`Now listening on port ${port}!`));
