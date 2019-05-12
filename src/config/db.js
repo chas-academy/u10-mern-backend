e8c187d7e79352f53b6e8ac9bc64b3aacf000ac8
Diar Kourehpaz <99diagha@gmail.com>
2019-05-12 20:50:48 +0200

    Refactor passport config.

--- src/config/db.js
+++ src/config/db.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-  .then(() => console.log('Connection to database successful'))
+  .then(() => console.log('Connection to database successful.'))
   .catch(err => console.log(err));
