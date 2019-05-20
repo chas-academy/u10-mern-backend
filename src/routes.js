1dd0539b0070ee44d411b4503dbcb1970b25df7e
Diar Kourehpaz <99diagha@gmail.com>
2019-05-20 19:53:30 +0200

    Nothing important.

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,25 @@ router.get(
   '/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
-    res.redirect(process.env.CLIENT_ORIGIN);
+    req.session.save(() => {
+      res.redirect(process.env.CLIENT_ORIGIN);
+    });
+  },
+);
+
+router.get(
+  '/user',
+  (req, res) => {
+    if (req.isAuthenticated()) {
+      const { name, email } = req.user.google;
+      return res.json({
+        user: {
+          name,
+          email,
+        },
+      });
+    }
+    return res.status(401).send('Not authenticated');
   },
 );
 
